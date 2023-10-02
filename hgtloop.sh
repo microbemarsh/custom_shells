7209b55e5cf3b2d3e2a3e8edb3d501325778db2d
Austin Marshall <75810734+microbemarsh@users.noreply.github.com>
2023-10-02 10:28:09 -0500

    Update hgtloop.sh

--- hgtloop.sh
+++ hgtloop.sh
@@ -18,13 +18,13 @@ echo "Created by Austin Marshall, Dept. of Biology @ Clarkson University. Built 
 mkdir soil_waafle
 DIR="$PWD"
 OUTDIR="$PWD/cooked_waafle"
-DB_DIR="/mnt/home/marshaag/Documents/waafle/waafledb/waafledb"
+DB_DIR="/your/path/to/waafle/waafledb/waafledb"
 
 # Running search to BLAST contig/s against waafle database
-for f in *contigs.fasta; do filename="${f%*.contigs.fasta}"; echo $f "Start Time: `date`"; /mnt/home/marshaag/waafle/bin/waafle_search $f "${DB_DIR}" --threads 80 ; echo $(date); done
+for f in *contigs.fasta; do filename="${f%*.contigs.fasta}"; echo $f "Start Time: `date`"; /your/path/to/waafle/bin/waafle_search $f "${DB_DIR}" --threads 80 ; echo $(date); done
 
 # Running genecaller to create .gff file from .blastout file
-for f in *contigs.blastout; do filename="${f%*.contigs.blastout}"; echo $f "Start Time: `date`"; /mnt/home/marshaag/waafle/bin/waafle_genecaller $f ; echo $(date); done
+for f in *contigs.blastout; do filename="${f%*.contigs.blastout}"; echo $f "Start Time: `date`"; /your/path/to/waafle/bin/waafle_genecaller $f ; echo $(date); done
 
 # Running orgscorer to find the LGT events, first we have to define an array of input prefixes
 input_prefixes=("s1" "s2" "s3" "s4" "s5" "s6")
@@ -35,14 +35,14 @@ for prefix in "${input_prefixes[@]}"; do
     fasta_file="${prefix}_contigs.fasta"
     blastout_file="${prefix}_contigs.blastout"
     gff_file="${prefix}_contigs.gff"
-    taxonomy_file="/mnt/home/marshaag/Documents/waafle/waafledb_taxonomy.tsv"
+    taxonomy_file="/your/path/to/waafle/waafledb_taxonomy.tsv"
 
     # Check if all required input files exist
     if [ -f "$fasta_file" ] && [ -f "$blastout_file" ] && [ -f "$gff_file" ]; then
         echo "Processing files for $prefix"
 
         # Run the waafle_orgscorer command with the input files
-        /mnt/home/marshaag/waafle/bin/waafle_orgscorer --basename "${prefix}" "$fasta_file" "$blastout_file" "$gff_file" "$taxonomy_file"
+        /your/path/to/waafle/bin/waafle_orgscorer --basename "${prefix}" "$fasta_file" "$blastout_file" "$gff_file" "$taxonomy_file"
         echo "Finished processing files for $prefix"
     else
         echo "Files not found for $prefix"
